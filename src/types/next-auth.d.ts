ff14becca1e3c1b8a01b568ea0214d30c0fa7724
ranaabdullahjaved <ranaabdullahjaved2003@gmail.com>
2025-07-11 10:58:13 -0600

    tsconfig.json + next-auth.d.ts modified

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,13 +1,14 @@
+// types/next-auth.d.ts
+import { DefaultSession, DefaultUser } from "next-auth";
+
 declare module "next-auth" {
   interface Session {
-    user?: {
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
+    user: {
       role?: string | null;
-    };
+    } & DefaultSession["user"];
   }
-  interface User {
+
+  interface User extends DefaultUser {
     role?: string | null;
   }
-} 
+}
